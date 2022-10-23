0f6b17b0733e646c5cb2ed3637687bd48702139b
indekkusu-era <lkycst@gmail.com>
2022-10-24 01:29:05 +0700

    wood

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 document.getElementById('unknown7').onclick = () => {
     window.open('https://osu.ppy.sh/beatmapsets/1863195#mania/3831323')
 }
+
+document.getElementById('unknown3').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1871847#mania/3851405')
+}
+
+document.getElementById('unknown4').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1871849#mania/3851407')
+}
+
+document.getElementById('unknown5').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1871846#mania/3851404')
+}
+
+localStorage.setItem('wood', 'wood')
