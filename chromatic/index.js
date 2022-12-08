a2d78707e8a55eb5cfe517b2a47b5a4fea01f395
indekkusu-era <lkycst@gmail.com>
2022-12-08 23:49:28 +0700

    chromatic

---
+++ chromatic/index.js
@@ -0,0 +1,15 @@
+document.getElementById('act-1-title').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1876155#mania/3860942')
+}
+
+document.getElementById('act-2-title').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1884776#mania/3880637')
+}
+
+document.getElementById('act-3-title').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1893947#mania/3902894')
+}
+
+document.getElementById('act-4-title').onclick = () => {
+    window.open('https://osu.ppy.sh/beatmapsets/1893944#mania/3902885')
+}
